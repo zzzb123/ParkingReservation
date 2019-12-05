7ff424257c2f6bf590624417adfc94bb64e9125e
KMankowski <mankowskikevin@gmail.com>
2019-12-05 10:19:33 -0500

    Added the FindCoordinates.java class to find the coordinates of builidings at GMU

---
+++ src/parkingclient/FindCoordinates.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class FindCoordinates {
+
+    public static String getCoordinates(String inputStr) throws IOException {
+        inputStr.replace(" ", "+");
+        URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=" + inputStr + ",+CA&key=AIzaSyAez0a7tgsZdb2Vuywbq_pWZ7UQNVveX1A");
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        InputStreamReader in = new InputStreamReader(conn.getInputStream());
+        int read;
+        char[] buff = new char[1024];
+        StringBuilder jsonStr = new StringBuilder();
+        while((read = in.read(buff)) != -1) {
+            jsonStr.append(buff, 0, read);
+        }
+        String latAndLng = "";
+        int i = jsonStr.indexOf("\"lat\" : ") + 8;
+        while(jsonStr.charAt(i) != ',') {
+            latAndLng += jsonStr.charAt(i);
+            i++;
+        }
+        latAndLng += ",";
+        int n = jsonStr.indexOf("\"lng\" : ") + 8;
+        while(Character.isDigit(jsonStr.charAt(n)) || jsonStr.charAt(n) == '-' || jsonStr.charAt(n) == '.') {
+            latAndLng += jsonStr.charAt(n);
+            n++;
+        }
+        return latAndLng;
+    }
+
+}
