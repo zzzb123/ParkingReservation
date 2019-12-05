e0267ee4de1bfb2424595fd85a5b6a8fbcb78f78
KMankowski <mankowskikevin@gmail.com>
2019-12-05 03:12:18 -0500

    Added Registration Scene

--- src/parkingclient/Main.java
+++ src/parkingclient/Main.java
@@ -8,18 +8,34 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    
+
+    static ServerInterconnect server = new ServerInterconnect();
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Parking Reservation");
+        loginMenu(primaryStage);
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void loginMenu(Stage primaryStage) {
 
         VBox overallDisplay = new VBox();
         VBox topDisplay = new VBox();
+
+        Label invalidUserNameWarning = new Label(" ");
+        topDisplay.getChildren().add(invalidUserNameWarning);
 
         Label loginText = new Label("Login");
         topDisplay.getChildren().add(loginText);
@@ -42,15 +58,94 @@ public class Main extends Application {
 
         overallDisplay.getChildren().addAll(topDisplay, bottomDisplay);
 
-        Scene loginScene = new Scene(overallDisplay);
-
+        Scene loginScene = new Scene(overallDisplay, 500, 350);
 
         primaryStage.setScene(loginScene);
         primaryStage.show();
+
+        registerButton.setOnAction(e -> {
+            registrationMenu(primaryStage);
+        });
+
+        loginButton.setOnAction(e -> {
+            if(server.setUser(usernameField.getText(), passwordField.getText())) {
+                mainMenu(primaryStage);
+            } else {
+                invalidUserNameWarning.setText("Warning: Incorrect Username or Password");
+            }
+        });
     }
 
+    public void registrationMenu(Stage primaryStage) {
 
-    public static void main(String[] args) {
-        launch(args);
+        VBox overallDisplay = new VBox();
+        VBox topDisplay = new VBox();
+
+        Label invalidWarning = new Label(" ");
+        topDisplay.getChildren().add(invalidWarning);
+
+        TextField usernameField = new TextField();
+        usernameField.setPromptText("Username");
+        topDisplay.getChildren().add(usernameField);
+
+        TextField emailField = new TextField();
+        emailField.setPromptText("Email");
+        topDisplay.getChildren().add(emailField);
+
+        TextField idField = new TextField();
+        idField.setPromptText("License Plate");
+        topDisplay.getChildren().add(idField);
+
+        TextField passwordField = new TextField();
+        passwordField.setPromptText("Password");
+        topDisplay.getChildren().add(passwordField);
+
+        TextField repasswordField = new TextField();
+        repasswordField.setPromptText("Re-Type Password");
+        topDisplay.getChildren().add(repasswordField);
+
+        HBox bottomDisplay = new HBox();
+
+        Button submitButton = new Button("Submit");
+        Button cancelButton = new Button("Cancel");
+
+        bottomDisplay.getChildren().addAll(submitButton, cancelButton);
+
+        overallDisplay.getChildren().addAll(topDisplay, bottomDisplay);
+
+        Scene registrationScene = new Scene(overallDisplay, 500, 350);
+        primaryStage.setScene(registrationScene);
+
+        submitButton.setOnAction(e -> {
+            if(passwordField.getText().equals(repasswordField.getText())) {
+                if(server.registerUser(usernameField.getText(), emailField.getText(), idField.getText(), passwordField.getText())) {
+                    mainMenu(primaryStage);
+                }
+                invalidWarning.setText("Warning: Username already exists");
+            } else {
+                invalidWarning.setText("Warning: Passwords do not Match");
+            }
+        });
+
+    }
+
+    public void mainMenu(Stage primaryStage) {
+        BorderPane overallLayout = new BorderPane();
+
+        VBox rightPane = new VBox();
+
+        HBox upperRight = new HBox();
+
+        TextField searchField = new TextField();
+        searchField.setPromptText("Closest lots to: ");
+
+        Button searchButton = new Button("Search");
+        upperRight.getChildren().addAll(searchButton, searchButton);
+
+        
+        overallLayout.setRight(rightPane);
+
+        Scene mainScene = new Scene(overallLayout);
+
     }
 }
