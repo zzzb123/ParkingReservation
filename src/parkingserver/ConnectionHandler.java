0c6a2c86aa7244ff30903c18f90747eb85436fbf
zzzb123 <zzzb123@gmail.com>
2019-12-04 22:07:40 -0500

    moving around code for the new client

--- src/parkingserver/ConnectionHandler.java
+++ src/parkingserver/ConnectionHandler.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map.Entry;
